ad406db05424ac695f26e708b7a677c2d311c09a
Lucio Palacio <luciopalacio95@gmail.com>
2024-09-10 10:40:42 -0300

    LP- add seo settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import { VitePWA } from "vite-plugin-pwa"
+
+// Helper imports
+import { manifest, seoConfig } from "./util/seoConfig"
 
 // https://astro.build/config
+
 export default defineConfig({
+	site: seoConfig.baseURL,
   integrations: [tailwind()],
   i18n: {
     locales: ['es', 'en'],
@@ -11,5 +17,22 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
    }
-  }
-});
+  },
+	vite: {
+		plugins: [
+			VitePWA({
+				registerType: "autoUpdate",
+				manifest,
+				workbox: {
+				  globDirectory: 'dist',
+				  globPatterns: [
+				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
+				  ],
+				  // Don't fallback on document based (e.g. `/some-page`) requests
+				  // This removes an errant console.log message from showing up.
+				  navigateFallback: null,
+				},
+			})
+		]
+	}
+})
