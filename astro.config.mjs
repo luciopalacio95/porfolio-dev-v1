0a7d2e64a78491f2747d9e2b3437940a1f390f56
Lucio Palacio <luciopalacio95@gmail.com>
2024-09-10 11:26:38 -0300

    Clear code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-import { VitePWA } from "vite-plugin-pwa"
-
-// Helper imports
-import { manifest, seoConfig } from "./util/seoConfig"
 
 // https://astro.build/config
 
 export default defineConfig({
-	site: seoConfig.baseURL,
   integrations: [tailwind()],
   i18n: {
     locales: ['es', 'en'],
@@ -18,21 +12,4 @@ export default defineConfig({
       prefixDefaultLocale: false
    }
   },
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
 })
